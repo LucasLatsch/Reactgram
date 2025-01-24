591424e9b825ad25b38aca18415a393ab090021e
Lucas Latsch <bello-lucas@hotmail.com>
2025-01-24 18:32:08 -0300

    Add deletePhoto functionality to remove photos with ownership validation

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -22,12 +22,39 @@ const insertPhoto = async (req, res) => {
 
   // If photo is not created
   if (!newPhoto) {
-    return res.status(422).json({ errors: "Photo could not be created" });
+    return res.status(422).json({ errors: ["Photo could not be created"] });
   }
 
   res.status(201).json(newPhoto);
 };
 
+// Remove a photo
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+
+  const reqUser = req.user;
+
+  try {
+    const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+
+    // Check if the photo exists
+    if (!photo) {
+      return res.status(404).json({ errors: ["Photo not found"] });
+    }
+
+    // Check if the user is the owner of the photo
+    if (!photo.userId.equals(reqUser._id)) {
+      return res.status(422).json({ errors: ["Try again later"] });
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    res.status(200).json({ id: photo._id, message: "Photo deleted" });
+  } catch (error) {
+    res.status(404).json({ id: photo._id, message: "Photo not found" });
+  }
+};
 module.exports = {
   insertPhoto,
+  deletePhoto,
 };
