57f834b4ada4e21e87c0cb516ffbc9f8419a8e4c
Lucas Latsch <bello-lucas@hotmail.com>
2025-01-25 19:18:42 -0300

    Add Navbar and Footer components, integrate react-redux, and enhance styling

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,9 +1,68 @@
 import "./Auth.css";
 
+// Components
+import { Link, Links } from "react-router-dom";
+
+// Hooks
+import { UseState, useEffect, useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
+
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
   return (
-    <div>
-      <h1>Register</h1>
+    <div id="register">
+      <h2>Reactgram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={name || ""}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email || ""}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password || ""}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          value={confirmPassword || ""}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Ja tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
     </div>
   );
 };
