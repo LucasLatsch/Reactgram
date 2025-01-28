f8fc488bc99d15c2820a9096ad578a511c4e3d39
Lucas Latsch <bello-lucas@hotmail.com>
2025-01-27 22:04:30 -0300

    Add login functionality and improve authentication flow

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,9 +1,44 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message/Message";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefalut();
+  };
   return (
     <div id="login">
-      <h1>Login</h1>
+      <h2>Reacgram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        <input type="submit" placeholder="Entrar" />
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">CLique aqui</Link>
+      </p>
     </div>
   );
 };
